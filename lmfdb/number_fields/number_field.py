6e177f7a38ee8c3990213f1130fda2dbee44ce8b
AndrewVSutherland <drew@math.mit.edu>
2020-10-16 16:53:44 -0400

    debug

--- lmfdb/number_fields/number_field.py
+++ lmfdb/number_fields/number_field.py
@@ -577,10 +577,9 @@ def render_field_webpage(args):
     label_orig = label
     if len(label) > 25:
         label = label[:16] + '...' + label[-6:]
-    print(D)
     properties = [('Label', label),
                   ('Degree', prop_int_pretty(data['degree'])),
-                  ('Signature', prop_int_pretty(data['signature'])),
+                  ('Signature', '$%s$' % data['signature']),
                   ('Discriminant', prop_int_pretty(D)),
                   ('Root discriminant', '%s' % data['rd']),
                   ('Ramified ' + primes + '', '$%s$' % ram_primes),
