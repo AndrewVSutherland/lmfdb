5748533f2a23289b532abe81e23d7f0b5c83227b
AndrewVSutherland <drew@math.mit.edu>
2020-10-16 20:55:03 -0400

    nf ram primes

--- lmfdb/number_fields/number_field.py
+++ lmfdb/number_fields/number_field.py
@@ -580,13 +580,17 @@ def render_field_webpage(args):
         primes = 'prime'
     else:
         primes = 'primes'
+    if len(ram_primes) > 30:
+        ram_primes = 'see page'
+    else:
+        ram_primes = '$%s$' % ram_primes
 
     properties = [('Label', nf_label_pretty(label)),
                   ('Degree', prop_int_pretty(data['degree'])),
                   ('Signature', '$%s$' % data['signature']),
                   ('Discriminant', prop_int_pretty(D)),
                   ('Root discriminant', '%s' % data['rd']),
-                  ('Ramified ' + primes + '', '$%s$' % ram_primes),
+                  ('Ramified ' + primes + '', ram_primes),
                   ('Class number', '%s %s' % (data['class_number'], grh_lab)),
                   ('Class group', '%s %s' % (data['class_group_invs'], grh_lab)),
                   ('Galois group', group_pretty_and_nTj(data['degree'], t))]
